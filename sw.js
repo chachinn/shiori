300b438a776808589edbcbddac2edff9233f22fd
chachinn <140357822+chachinn@users.noreply.github.com>
2026-09-10 04:10:01 +0800

    Refresh cache for pink Shiori theme

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='shiori-v3';
+const CACHE='shiori-v4';
 const ASSETS=['./','index.html','styles.css','data.js','app.js','manifest.webmanifest','icons/app-icon-128.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
